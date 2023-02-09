54a1a7a98228cf23745a8a6d6acc5cbfb35cc8e3
reachify <60685625+Trtim@users.noreply.github.com>
2023-02-09 19:42:34 +0800

    Add files via upload

---
+++ setScore.js
@@ -0,0 +1,19 @@
+(async () => {
+    let n = document.createElement('iframe');
+    document.body.append(n);
+    window.alert = n.contentWindow.alert.bind(window);
+    window.prompt = n.contentWindow.prompt.bind(window);
+    window.confirm = n.contentWindow.confirm.bind(window);
+    n.remove(); 
+
+    if (window.location.hostname != 'edu.chinese-stories.com') {
+        alert('You must be in Chinese stories to use this!');
+    } else {
+        let percent = Number(parseFloat(prompt('What percent do u want?')));
+        if (percent > 100) {
+            alert('Percent must be less than 100');
+        } else {
+            save_tests(percent , 5);
+        }
+    };
+})();
