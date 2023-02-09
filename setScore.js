22f43780d0f15561a02ad39b04c29e99d26c89cc
reachify <60685625+Trtim@users.noreply.github.com>
2023-02-09 20:16:16 +0800

    Update setScore.js

--- setScore.js
+++ setScore.js
@@ -5,6 +5,12 @@
     window.prompt = n.contentWindow.prompt.bind(window);
     window.confirm = n.contentWindow.confirm.bind(window);
     n.remove(); 
+    
+   function getRandomInt(min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min) + min);
+    }
 
     if (window.location.hostname != 'edu.chinese-stories.com') {
         alert('You must be in Chinese stories to use this!');
@@ -13,6 +19,7 @@
         if (percent > 100) {
             alert('Score cannot be over 100');
         } else {
+            timeSpentOnPage = getRandomInt(224,439);
             save_tests(percent , 5);
         }
     };
